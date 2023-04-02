210dc839d19ab8b26b3e95eef549f1acba6f1bc7
Daniil Gorbachev <Daniil1938@yandex.ru>
2023-04-02 21:55:04 +0300

    commit

--- src/test/java/ru/mirea/TrppTest.java
+++ src/test/java/ru/mirea/TrppTest.java
@@ -2,10 +2,10 @@ package ru.mirea;
 
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
-import org.junit.jupiter.api.Test;
+import jakarta.inject.Inject;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import javax.inject.Inject;
 
 @MicronautTest
 class TrppTest {
